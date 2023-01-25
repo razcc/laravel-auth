bffbfad5b6768ab78d4d332640616de10d6c0f83
Rafael Leonardi <rafael.leonardi98@gmail.com>
2023-01-25 19:54:08 +0100

    Creazione Progetto, con inserimento vue

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+
+
+// Gestione Rotte sotto Autenticazione
+Route::middleware('auth')->namespace('Admin')->prefix('admin')->name('admin.')->group(function () {
+
+    Route::get('/', 'HomeController@index')->name('index');
 });
+
+//Gestione rotte guest
+Route::get('{any?}', function () {
+
+    return view('guest.home');
+    
+})->where("any", ".*");
